c2cce0222ad7f13c723e9db33069f4fbb0021db0
kerbal <tranquockhanh997@gmail.com>
2020-06-24 14:06:29 +0700

    init

---
+++ packages/api-server/config/config.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: 'localhost',
+    dialect: 'postgres',
+  },
+  test: {
+    username: process.env.CI_DB_USERNAME,
+    password: process.env.CI_DB_PASSWORD,
+    database: process.env.CI_DB_NAME,
+    host: 'localhost',
+    dialect: 'postgres',
+    logging: false,
+  },
+  production: {
+    username: process.env.PROD_DB_USERNAME,
+    password: process.env.PROD_DB_PASSWORD,
+    database: process.env.PROD_DB_NAME,
+    host: process.env.PROD_DB_HOSTNAME,
+    dialect: 'postgres',
+    logging: false,
+  },
+};
