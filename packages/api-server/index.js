904f2d2e05dfbf009785da58367d65fa42ba67af
kerbal <tranquockhanh997@gmail.com>
2020-06-24 14:22:45 +0700

    init

---
+++ packages/api-server/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import models from './models';
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((err, req, res, next) => {
+  console.log(err.message);
+});
+
+app.listen(process.env.PORT || 3000, async () => {
+  models.sequelize.authenticate().then(() => {
+    console.log('Database connected!');
+    console.log('Server is up!');
+  });
+});
