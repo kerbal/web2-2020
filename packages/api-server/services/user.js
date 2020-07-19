7bed2d9f1dc3f0f835e0d53df3258b50e13fe17e
Hoang Thuc <hoangthuc701@gmail.com>
2020-07-19 21:33:01 +0700

    update userservice

--- packages/api-server/services/user.js
+++ packages/api-server/services/user.js
@@ -2,33 +2,55 @@ import { Customer } from '../models';
 import { Sequelize } from '../models/';
 
 class UserService {
-  static async getAllUser(limit, page) {
+  static async getAllUser(limit=10, page=1) {
     const offset = (page - 1) * limit;
     const users = await Customer.findAll({
       offset,
       limit,
+      attributes: ['id', 'fullname',  'email', 'birthday', 'phone_number', 'address'],
     });
+    if (!users) return [];
     return users;
   }
-  static async search(pattern){
+  static async getById(id){
+    try{
+      const user = await Customer.findOne({
+        where:{
+          id,
+        },
+        attributes: ['id', 'fullname',  'email', 'birthday', 'phone_number', 'address'],
+      });
+      if (!user) return {};
+      return user;
+    }
+    catch(error){
+      return {};
+    }
+
+  }
+  static async search(pattern, limit=10, page=1){
+    const offset = (page - 1) * limit;
     try{
       const users = await Customer.findAll({
-        attributes: ['fullname', 'id', 'email'],
+        attributes: ['id', 'fullname', 'email', 'birthday', 'phone_number', 'address'],
         where:{
           [Sequelize.Op.or]:[
             {
               email:{
-                [Sequelize.Op.like]: `%${pattern}%`,
+                [Sequelize.Op.iLike]: `%${pattern}%`,
               },
             },
             {
               fullname:{
-                [Sequelize.Op.like]: `%${pattern}%`,
+                [Sequelize.Op.iLike]: `%${pattern}%`,
               },
             },
           ],
         },
+        offset,
+        limit,
       });
+      if (!users) return [];
       return users;
     }
     catch(error){
