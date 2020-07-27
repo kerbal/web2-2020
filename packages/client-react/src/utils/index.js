2d888aed1ccee84ad8b91733b62386c0cba9f586
huynonstop <tthuykh99@gmail.com>
2020-07-27 11:44:26 +0700

    fix eslint

--- packages/client-react/src/utils/index.js
+++ packages/client-react/src/utils/index.js
@@ -1,25 +1,36 @@
-import moment from 'moment';
+import Moment from 'moment';
 
 export const getCurrentDatetime = () => {
-  return new moment().format("DD/MM/YYYY HH:mm");
-}
+  return new Moment().format('DD/MM/YYYY HH:mm');
+};
 
-export const formatDatetime = (datetime) => {
-  return new moment(datetime).format("DD/MM/YYYY HH:mm");
-}
+export const formatDatetime = datetime => {
+  return new Moment(datetime).format('DD/MM/YYYY HH:mm');
+};
 
-export const formatCurrency = (amount, decimalCount = 2, decimal = ".", thousands = ",") => {
-  try {
-    decimalCount = Math.abs(decimalCount);
-    decimalCount = isNaN(decimalCount) ? 2 : decimalCount;
+export const formatCurrency = (
+  amount,
+  decimalCount = 2,
+  decimal = '.',
+  thousands = ','
+) => {
+  let absDecimalCount = Math.abs(decimalCount);
+  absDecimalCount = Number.isNaN(absDecimalCount) ? 2 : absDecimalCount;
 
-    const negativeSign = amount < 0 ? "-" : "";
+  const negativeSign = amount < 0 ? '-' : '';
+  const absAmount = Math.abs(Number(amount) || 0).toFixed(absDecimalCount);
+  const i = parseInt(absAmount, 10).toString();
+  const j = i.length > 3 ? i.length % 3 : 0;
 
-    let i = parseInt(amount = Math.abs(Number(amount) || 0).toFixed(decimalCount)).toString();
-    let j = (i.length > 3) ? i.length % 3 : 0;
-
-    return negativeSign + (j ? i.substr(0, j) + thousands : '') + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousands) + (decimalCount ? decimal + Math.abs(amount - i).toFixed(decimalCount).slice(2) : "");
-  } catch (e) {
-    console.log(e)
-  }
-}
+  return (
+    negativeSign +
+    (j ? i.substr(0, j) + thousands : '') +
+    i.substr(j).replace(/(\d{3})(?=\d)/g, `$1${thousands}`) +
+    (decimalCount
+      ? decimal +
+        Math.abs(amount - i)
+          .toFixed(decimalCount)
+          .slice(2)
+      : '')
+  );
+};
