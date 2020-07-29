49544263335c159382e3bd86103079728d46bbb1
kerbal <tranquockhanh997@gmail.com>
2020-07-29 16:51:41 +0700

    finish create transaction

--- packages/client-react/src/components/pages/Dashboard/pages/Transfer/TransferComponent.js
+++ packages/client-react/src/components/pages/Dashboard/pages/Transfer/TransferComponent.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import _ from 'lodash';
 import ComboBox from '../../../../common/ComboBox';
 import Input from '../../../../common/Input';
 import VerifyTransaction from './VerifyTransaction';
@@ -8,7 +10,7 @@ const Container = ({ children }) => {
   return <div className="flex-1 p-6">{children}</div>;
 };
 
-const Transfer = () => {
+const Transfer = props => {
   const accounts = [
     {
       id: 0,
@@ -21,24 +23,27 @@ const Transfer = () => {
       balance: 225000,
     },
   ];
+  const { refresh } = props;
   const [currentAccountIndex, setCurrentAccountIndex] = useState(1);
   const [accountId, setAccountId] = useState(accounts[currentAccountIndex].id);
   const [destinationAccountNumber, setDestinationAccountNumber] = useState();
   const [amount, setAmount] = useState(0);
   const [note, setNote] = useState('');
-  const [sourceBankId, setSourceBankId] = useState('PIGGY');
+  // const [sourceBankId, setSourceBankId] = useState('PIGGY');
   const [destinationBankId, setDestinationBankId] = useState('PIGGY');
   const [creating, setCreating] = useState(false);
   const [verify, setVerify] = useState(false);
-  const [transaction, setTransaction] = useState(null);
+  const [transactionId, setTransactionId] = useState(null);
   const [error, setError] = useState(null);
   const [destinationAccount, setDestinationAccount] = useState(null);
+  const token = useSelector(state => state.customerAuth.token);
 
-  const searchDestinationAccount = async () => {
-    const res = await axios.get(
-      `/customer/account?account_number=${destinationAccountNumber}`
-    );
-    console.log(res.data);
+  const searchDestinationAccount = async value => {
+    const res = await axios.get(`/customer/account?account_number=${value}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
     if (res.data.length === 1) {
       setDestinationAccount(res.data[0]);
     }
@@ -46,14 +51,29 @@ const Transfer = () => {
 
   const createTransaction = async () => {
     setError(null);
-    const res = await axios.post('/transaction', {
-      source_bank_id: 'PIGGY',
-      destination_bank_id: destinationBankId,
-      source_account_id: accountId,
-      destination_account_id: '',
-      amount,
-      note,
-    });
+    try {
+      const res = await axios.post(
+        '/transaction',
+        {
+          source_bank_id: 'PIGGY',
+          destination_bank_id: destinationBankId,
+          source_account_id: accountId,
+          destination_account_id: destinationAccount.id,
+          amount,
+          note,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      setTransactionId(res.data.transaction_id);
+      setVerify(true);
+    } catch (err) {
+      setError(_.get(err, 'response.data.message') || err.message);
+    }
+    setCreating(false);
   };
 
   return (
@@ -83,7 +103,7 @@ const Transfer = () => {
           />
         </div>
       </div>
-      <div className="flex">
+      <div className="flex mb-6">
         <div className="w-1/3">
           <ComboBox
             label="Destination Bank"
@@ -102,13 +122,32 @@ const Transfer = () => {
             value={destinationAccountNumber}
             label="Destination Account Number"
             disabled={verify}
-            onValueChange={value => {
+            onValueChange={async value => {
               setDestinationAccountNumber(value);
-              searchDestinationAccount();
+              if (`${value}`.length === 16) {
+                searchDestinationAccount(value);
+              }
             }}
           />
         </div>
       </div>
+      {destinationAccount && (
+        <div>
+          <h3 className="text-lg font-semibold mb-2">Destination account</h3>
+          <p className="mb-1">
+            <span className="font-semibold mr-2">Name:</span>
+            {destinationAccount.Customer.fullname}
+          </p>
+          <p className="mb-1">
+            <span className="font-semibold mr-2">Email:</span>
+            {destinationAccount.Customer.email}
+          </p>
+          <p className="mb-1">
+            <span className="font-semibold mr-2">Phone number:</span>
+            {destinationAccount.Customer.phone_number}
+          </p>
+        </div>
+      )}
       <div className="w-2/3">
         <Input
           value={amount}
@@ -127,19 +166,24 @@ const Transfer = () => {
         />
       </div>
       {!verify ? (
-        <button
-          className="btn bg-blue-900 text-white w-2/3 rounded shadow-md text-lg p-2 button"
-          type="button"
-          disabled={creating}
-          onClick={() => {
-            setVerify(true);
-            setCreating(true);
-          }}
-        >
-          Make transfer
-        </button>
+        <div className="w-2/3">
+          {destinationAccount && (
+            <button
+              className="btn bg-blue-900 text-white rounded shadow-md text-lg p-2 button mb-4 w-full"
+              type="button"
+              disabled={creating}
+              onClick={() => {
+                setCreating(true);
+                createTransaction();
+              }}
+            >
+              Make transfer
+            </button>
+          )}
+          {error && <p className="text-lg text-red-500">{error}</p>}
+        </div>
       ) : (
-        <VerifyTransaction transaction={transaction} />
+        <VerifyTransaction transactionId={transactionId} refresh={refresh} />
       )}
     </Container>
   );
--- packages/client-react/src/components/pages/Dashboard/pages/Transfer/TransferContainer.js
+++ packages/client-react/src/components/pages/Dashboard/pages/Transfer/TransferContainer.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TransferComponent from './TransferComponent';
 import withDashboardFrame from '../../withDashboardFrame';
 import withProtected from '../../withProtected';
 
 const TransferContainer = () => {
-  return <TransferComponent />;
+  const [refresh, setRefresh] = useState(false);
+
+  if (refresh) {
+    return <div />;
+  }
+
+  return (
+    <div className="w-full">
+      <TransferComponent
+        refresh={async () => {
+          console.log('ble');
+          await setRefresh(true);
+          await setRefresh(false);
+        }}
+      />
+    </div>
+  );
 };
 
 export default withProtected()(withDashboardFrame(TransferContainer));
